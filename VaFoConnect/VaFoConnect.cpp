cf843b2fb760617fa5440795364c26fdd895e4c2
Kireino <matgizq@gmail.com>
2016-04-05 20:06:42 +0200

    Added submodule, Added Sys variables for Boost (BOOST_PATH) and Asio (ASIO_PATH), configured project includes

--- VaFoConnect/VaFoConnect.cpp
+++ VaFoConnect/VaFoConnect.cpp
@@ -2,10 +2,21 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+#include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/server.hpp>
 
 int main()
 {
-    return 0;
-}
+	int z;
+	std::cout << "Hello, you b-baka!" << std::endl;
 
+	system("PAUSE");
+	return 0;
+	/*using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");*/
+}
